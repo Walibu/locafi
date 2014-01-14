8c226c2409bf6f334e8830d56a6b8fc603294c1b
WaliBu <walter.buerki@gmail.com>
2014-01-14 19:41:25 +0100

    Support for 4.0 and lower on: Respond to the action bar's Up/Home button

--- src/ch/buerkigiger/locationfinder/AboutActivity.java
+++ src/ch/buerkigiger/locationfinder/AboutActivity.java
@@ -2,6 +2,8 @@ package ch.buerkigiger.locationfinder;
 
 import ch.buerkigiger.locationfinder.R;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.app.Activity;
 
 
@@ -12,5 +14,16 @@ public class AboutActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		// Respond to the action bar's Up/Home button
+		case android.R.id.home:
+	    	NavUtils.navigateUpFromSameTask(this);
+	    	return true;
+	    }
+	    return super.onOptionsItemSelected(item);
 	}
 }
